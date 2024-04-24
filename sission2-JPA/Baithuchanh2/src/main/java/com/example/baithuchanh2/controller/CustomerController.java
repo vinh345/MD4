5e93878bfa2bcd6cfb26e5abbd57d3cc1482bc5b
vinh345 <vinh77r@gmail.com>
2024-04-24 08:55:42 +0700

    htrht

---
+++ sission2-JPA/Baithuchanh2/src/main/java/com/example/baithuchanh2/controller/CustomerController.java
@@ -0,0 +1,85 @@
+package com.example.baithuchanh2.controller;
+
+import com.example.baithuchanh2.model.Customer;
+import com.example.baithuchanh2.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+
+@Controller
+public class CustomerController {
+    @Autowired
+    private ICustomerService customerService;
+
+    @GetMapping("/create-customer")
+    public ModelAndView showCreateForm() {
+        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        modelAndView.addObject("customer", new Customer());
+        return modelAndView;
+    }
+
+    @PostMapping("/create-customer")
+    public ModelAndView saveCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        modelAndView.addObject("customer", new Customer());
+        modelAndView.addObject("message", "New customer created successfully");
+        return modelAndView;
+    }
+
+    // hiển thị
+    @GetMapping("/customers")
+    public ModelAndView listCustomers() {
+        ModelAndView modelAndView = new ModelAndView("/customer/list");
+        modelAndView.addObject("customers", customerService.findAll());
+        return modelAndView;
+    }
+
+    // cập nhật
+    @GetMapping("/edit-customer/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id) {
+        Optional<Customer> customer = customerService.findById(id); // lấy đối tượng từ data base
+        if (customer.isPresent()) { // tồn tại
+            ModelAndView modelAndView = new ModelAndView("/customer/edit"); // tạo form edit
+            modelAndView.addObject("customer", customer.get()); // thêm dữ liêu vào form
+            return modelAndView;
+        } else {
+            return new ModelAndView("/error.404");
+        }
+    }
+
+    @PostMapping("/edit-customer")
+    public ModelAndView updateCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("/customer/edit");
+        modelAndView.addObject("customer", customer);
+        modelAndView.addObject("message", "Customer updated successfully");
+        return modelAndView;
+    }
+
+    // xóa
+    @GetMapping("/delete-customer/{id}")
+    public ModelAndView showDeleteForm(@PathVariable Long id) {
+        Optional<Customer> customer = customerService.findById(id);
+        if (customer.isPresent()) {
+            ModelAndView modelAndView = new ModelAndView("/customer/delete");
+            modelAndView.addObject("customer", customer.get());
+            return modelAndView;
+
+        } else {
+            return new ModelAndView("/error.404");
+        }
+    }
+    // xóa
+    @PostMapping("/delete-customer")
+    public String deleteCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.remove(customer.getId());
+        return "redirect:customers";
+    }
+}
---
+++ sission2-JPA/Baithuchanh2/src/main/java/com/example/baithuchanh2/service/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.example.baithuchanh2.service;
+
+import com.example.baithuchanh2.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+
+}
